34f0fc2fb6c2d09dbb7263cc8af388a4981423c1
kac- <6b6163@gmail.com>
2014-09-25 09:37:33 +0200

    ukernel peformance improvement

--- ukernel.go
+++ ukernel.go
@@ -1,24 +1,17 @@
 package umint
 
 import (
-	"bytes"
-	"encoding/binary"
 	"errors"
 	"github.com/conformal/fastsha256"
 	"math/big"
 )
 
 const (
-	stakeMaxAge int64 = 60 * 60 * 24 * 90
+	day         int   = 60 * 60 * 24
+	stakeMaxAge int64 = 90 * int64(day)
 	coin        int64 = 1000000
+	coinDay     int64 = coin * int64(day)
 )
-
-func minInt64(a int64, b int64) int64 {
-	if a < b {
-		return a
-	}
-	return b
-}
 
 func doubleSha256(b []byte) []byte {
 	hasher := fastsha256.New()
@@ -58,19 +51,21 @@ func compactToBig(compact uint32) *big.Int {
 }
 
 type StakeKernelTemplate struct {
-	Bits           uint32
-	BlockFromTime  int64
-	TxTime         int64
-	StakeModifier  uint64
-	PrevTxOffset   uint32
-	PrevTxTime     int64
+	BlockFromTime int64
+	StakeModifier uint64
+	PrevTxOffset  uint32
+	PrevTxTime    int64
+
 	PrevTxOutIndex uint32
 	PrevTxOutValue int64
-	IsProtocolV03  bool
-	StakeMinAge    int64
+
+	IsProtocolV03 bool
+	StakeMinAge   int64
+	Bits          uint32
+	TxTime        int64
 }
 
-func CheckStakeKernelHash(t StakeKernelTemplate) (hashProofOfStake []byte, success bool, err error) {
+func CheckStakeKernelHash(t *StakeKernelTemplate) (hashProofOfStake []byte, success bool, err error) {
 	success = false
 
 	if t.TxTime < t.PrevTxTime { // Transaction timestamp violation
@@ -84,51 +79,65 @@ func CheckStakeKernelHash(t StakeKernelTemplate) (hashProofOfStake []byte, succe
 	}
 
 	bnTargetPerCoinDay := compactToBig(t.Bits)
-
 	var timeReduction int64
 	if t.IsProtocolV03 {
 		timeReduction = t.StakeMinAge
 	} else {
 		timeReduction = 0
 	}
-	var nTimeWeight int64 = minInt64(t.TxTime-t.PrevTxTime, stakeMaxAge) - timeReduction
 
-	var bnCoinDayWeight *big.Int = new(big.Int).Div(new(big.Int).
-		Div(
-		new(big.Int).Mul(big.NewInt(t.PrevTxOutValue), big.NewInt(nTimeWeight)),
-		new(big.Int).SetInt64(coin)),
-		big.NewInt(24*60*60))
+	var nTimeWeight int64 = t.TxTime - t.PrevTxTime
+	if nTimeWeight > stakeMaxAge {
+		nTimeWeight = stakeMaxAge
+	}
+	nTimeWeight -= timeReduction
 
-	buf := bytes.NewBuffer(make([]byte, 0, 28)) // TODO pre-calculate size?
+	var bnCoinDayWeight *big.Int
+	valueTime := t.PrevTxOutValue * nTimeWeight
+	if valueTime > 0 { // no overflow
+		bnCoinDayWeight = new(big.Int).SetInt64(valueTime / coinDay)
+	} else {
+		// overflow, calc w/ big.Int or return error?
+		err = errors.New("valueTime overflow")
+		return
+		bnCoinDayWeight = new(big.Int).Div(new(big.Int).
+			Div(
+			new(big.Int).Mul(big.NewInt(t.PrevTxOutValue), big.NewInt(nTimeWeight)),
+			new(big.Int).SetInt64(coin)),
+			big.NewInt(24*60*60))
+	}
+	targetInt := new(big.Int).Mul(bnCoinDayWeight, bnTargetPerCoinDay)
+
+	buf := [28]byte{}
+	o := 0
 
 	if t.IsProtocolV03 { // v0.3 protocol
-		err = binary.Write(buf, binary.LittleEndian, t.StakeModifier)
-		if err != nil {
-			return
+		d := t.StakeModifier
+		for i := 0; i < 8; i++ {
+			buf[o] = byte(d & 0xff)
+			d >>= 8
+			o++
 		}
 	} else { // v0.2 protocol
-		//ss << Bits;
-		err = binary.Write(buf, binary.LittleEndian, t.Bits)
-		if err != nil {
-			return
+		d := t.Bits
+		for i := 0; i < 4; i++ {
+			buf[o] = byte(d & 0xff)
+			d >>= 8
+			o++
 		}
 	}
 	data := [5]uint32{uint32(t.BlockFromTime), uint32(t.PrevTxOffset),
 		uint32(t.PrevTxTime), uint32(t.PrevTxOutIndex), uint32(t.TxTime)}
 	for _, d := range data {
-		err = binary.Write(buf, binary.LittleEndian, d)
-		if err != nil {
-			return
+		for i := 0; i < 4; i++ {
+			buf[o] = byte(d & 0xff)
+			d >>= 8
+			o++
 		}
 	}
-
-	hashProofOfStake = doubleSha256(buf.Bytes())
-	if err != nil {
-		return
-	}
-
+	hashProofOfStake = doubleSha256(buf[:o])
 	hashProofOfStakeInt := new(big.Int).SetBytes(hashProofOfStake)
-	targetInt := new(big.Int).Mul(bnCoinDayWeight, bnTargetPerCoinDay)
+
 	if hashProofOfStakeInt.Cmp(targetInt) > 0 {
 		return
 	}
